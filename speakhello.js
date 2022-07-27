3887f2792f9ac5aaf15893a06f1a46af4ef25fbb
NELSONRAJ2002 <109501100+NELSONRAJ2002@users.noreply.github.com>
2022-07-27 18:41:11 +0530

    update

--- speakhello.js
+++ speakhello.js
@@ -1,19 +1,11 @@
 
-(function (window) {
-    // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-  // See Lecture 52, part 2
-  
-  
-  var helloSpeaker = {};
-  
- 
-    var speakWord = "Hello";
-  
-    helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
-    };
-  
-    window.helloSpeaker = helloSpeaker;
-  
-  })(window);
-  
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
